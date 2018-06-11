8a587676a1b7422dbee341a88ed056cbaac7b32c
Khong <kong_216@hotmail.com>
2018-06-11 09:53:02 +0700

    Add yes/no as the attribute in Question

--- quiz/models.py
+++ quiz/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 class Question(models.Model):
     question_text = models.CharField(max_length = 200)
+    say_yes = models.IntegerField(default=0)
+    say_no = models.IntegerField(default=0)
 
     def __str__(self):
         return self.question_text
